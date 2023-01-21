a3152d2ed221d5bab2bd604ec7598f44b059b967
SakthivelMurugan <sakthi255vel@gmail.com>
2023-01-21 08:50:33 +0530

    adding program

---
+++ ex11.py
@@ -0,0 +1,103 @@
+# string="sakthi"
+# s=string
+# i=len(string)-2
+# while i>=0:
+#     s+=string[i]
+#     i=i-1
+
+# ss=0
+# e=len(s)-1
+# for i in range(len(s)):
+#     for j in range(len(s)):
+#         if j==ss or j==e:
+#             print(s[j],end="")
+#         else:
+#             print(" ",end="")
+#     else:
+#         ss+=1
+#         e-=1
+#         print()
+
+# string="sakthi"
+# x=0
+# y=len(string)*2-2
+# k=0
+# for i in range(len(string)*2-1):
+#     if k==len(string):
+#         k=-2
+#     for j in range(len(string)*2-1):
+#         if j==x or j==y:
+#             print(string[k],end="")
+#         else:
+#             print(" ",end="")
+#     else:
+#         print()
+#         if k>=0:
+#             k+=1
+#         else:
+#             k-=1
+#         x+=1
+#         y-=1
+
+# l=["sarthi","sar","sar"]
+# l2=[]
+# for i in l:
+#     l2.append(len(i))
+# s=min(l2)
+# s1=l[0]
+# i=0
+# a=0
+# while i<s:
+#     j=0
+#     while j<len(l):
+#         if s1[i]==l[j][i]:
+#             count=True
+#         else:
+#             count=False
+#             j=len(l)-1
+#         j+=1
+#     if count==True:
+#         a+=1
+#     i+=1
+# print(a)
+
+# a=5
+# i=0
+# while i<=a:
+#     if i==a:
+#         x=0
+#         while x<a:
+#             y=0
+#             while y<a:
+#                 if x>=y:
+#                     print(" ",end="")
+#                 else:
+#                     print("*",end="")
+#                 y+=1
+#             x+=1
+#             print()
+#         i+=1
+
+#     else:   
+#         j=0
+#         while j<a:
+#             if i>=j:
+#                 print("*",end="")
+#             else:
+#                 print(" ",end="")
+#             j+=1
+#         i+=1
+#         print()
+    
+
+
+
+
+
+
+        
+
+
+
+
+
