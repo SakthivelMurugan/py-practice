d3a2275e823fcd94a6e2740fd82f7986f5d16086
SakthivelMurugan <sakthi255vel@gmail.com>
2023-01-25 04:59:53 +0530

    updated.

--- ex15.py
+++ ex15.py
@@ -1,12 +1,28 @@
+# n=5
+# for i in range(n):
+#     j=0
+#     while j<n:
+#         if i==0 or j>=i:
+#             print("*",end="")
+#             j+=1
+#         else:
+#             print(" ",end="")
+#             j+=1
+#     else:
+#         print()
+
 n=5
-for i in range(n):
+i=0
+a=n-1
+while i<n:
     j=0
     while j<n:
-        if i==0 or j>=i:
+        if j==a or j>a:
             print("*",end="")
-            j+=1
         else:
             print(" ",end="")
-            j+=1
+        j+=1
     else:
         print()
+        i+=1
+        a-=1
