6d5173e7e78d40bb38f9f09c77b032deb2f4ada5
SakthivelMurugan <sakthi255vel@gmail.com>
2023-02-05 04:29:17 +0530

    adding problem

--- ex18.py
+++ ex18.py
@@ -1,7 +1,61 @@
 # sakthi="sakthi"
 # print(dir(sakthi))
 
+# x=1,2
+# print(type(x))
+
+# name="sakthi"
+# n=name[-3:-1]
+# print(n)
+
+# print(bool(""))
+
+# a=[1,2,3]
+# b=[1,2,3]
+# #c=a
+# b.insert(1,2)
+# print(b)
+# # print(b is c)
+# if a==b:
+#     print("same")
+# else:
+#     print("not same")
 
 
-x=list([1])
-print(type(x))
+
+# s="abc"
+# s1='abc'
+# print(s is s1)
+
+# y=[1,2,3]
+# z=['a','b','c']
+# print(y+z)
+# y.extend(z)
+# print(y)
+# print(z)
+
+
+# a,b,*c=1,2,3,4,5
+# c=set(c)
+# c.remove(3)
+# c.discard(3)
+# print(c)
+
+# s=set([1,2,3])
+# print(type(s))
+
+# dic={"s":"sss","a":"aaa"}
+# for i,j in dic.items():
+#     print(i)
+#     print(j)
+
+# a=10
+# b=5
+# print("yes") if a>b else print("no")
+# if a>b: print(a)
+s="sakthivel"
+for i in s:
+    if i=='i':
+        continue
+        print(i)
+        
---
+++ ex19.py
@@ -0,0 +1,18 @@
+# x=[1,2,3,4,5]
+# op=[i for i in x if i%2!=0]
+# print(op)
+# op2=[i if i%2!=0 else i*2 for i in x]
+# print(op2) 
+
+
+
+
+# print("yes") if 2%2!=0 else print("no")
+
+
+
+# if 2/2==1: print("yes")
+
+
+
+
